607689222f29f817b48c23bc77d9e3ab857363b2
YashynYan <yanik1236@gmail.com>
2022-01-14 14:03:50 +0200

    added main container

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { SearchBar } from "../SearchBar/SearchBar";
+
+import './Main.css'
+
+export const Main = () => {
+  return (
+    <main>
+      <SearchBar />
+    </main>
+  );
+};
