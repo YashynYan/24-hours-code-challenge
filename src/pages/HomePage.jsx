6088fb21c0d1f37d93850c384dbc2dbdc9d01050
YashynYan <yanik1236@gmail.com>
2022-01-14 13:32:49 +0200

    created HomePage

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Header } from "../components/Header";
+import { Main } from "../components/Main/Main";
+
+export const HomePage = () => {
+  return (
+    <div>
+      <Header />
+      <Main />
+    </div>
+  );
+};
